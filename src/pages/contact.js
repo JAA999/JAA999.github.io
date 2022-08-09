bec79ae5b6952452dd60a103280c557f4d2e5c16
JAA999 <JArteaga999@icloud.com>
2022-08-08 18:57:37 -0600

    Created structure for the three pages on navbar

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './contact.css';
+
+const Contact = () => {
+    return (
+        <div className='Contact'>
+            <h1 className='Contact-title'>Contact</h1>
+        </div>
+    );
+};
+
+export default Contact;
