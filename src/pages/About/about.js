995a026e61c72921bd31104281c731bea5eb5c7e
JAA999 <JArteaga999@icloud.com>
2022-08-13 14:08:43 -0600

    Added title and image

--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -3,8 +3,11 @@ import './about.css';
 
 const About = () => {
     return(
-        <div>
-            <h1>About</h1>
+        <div className='about-page'>
+            <div className='header'>
+                <img src='./developing.jpg' className='about-image'/>
+                <h1 className='about-title'>About</h1>
+            </div>
         </div>
     );
 };
