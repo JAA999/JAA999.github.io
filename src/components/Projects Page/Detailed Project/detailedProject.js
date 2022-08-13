7c1f227ff8eba200b47939fb721d0168950016f6
JAA999 <JArteaga999@icloud.com>
2022-08-13 13:42:16 -0600

    Added bare-bones information

---
+++ src/components/Projects Page/Detailed Project/detailedProject.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './detailedProject.css';
+import { IconContext } from 'react-icons';
+import {AiFillCloseCircle} from 'react-icons/ai';
+
+class DetailedProject extends React.Component{
+    render(){
+        return(
+            <div className='detailed-project-cont'>
+                <p>Hello World</p>
+            </div>
+        );
+    }
+}
+
+export default DetailedProject;
