2d391d87ba2810356d6f8d90a1bfee44882621b1
JAA999 <JArteaga999@icloud.com>
2022-08-13 20:56:06 -0600

    Added basic content

--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -5,8 +5,13 @@ const About = () => {
     return(
         <div className='about-page'>
             <div className='header'>
-                <img src='./developing.jpg' className='about-image'/>
-                <h1 className='about-title'>About</h1>
+                <img src='./developing.jpg' className='about-page-image'/>
+                <div className='about-page-intro'>
+                    <h1 className='about-page-title'>About</h1>
+                    <p>A few years ago I took my first programming class in high school, and despite the
+                        struggle in class, I developed a deep interest in programming.
+                    </p>
+                </div>
             </div>
         </div>
     );
